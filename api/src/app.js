8514b4ab0ae473474bdd05f70bead472733065a7
pradeepOP <pradeepkazi38@gmail.com>
2024-06-19 22:55:51 +0545

    cloudinary and multer setup

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { tr } from "date-fns/locale";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export default app;
